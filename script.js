502371bebbe87d613cbcdf7d0537b20b446a0604
Ajagbe Gideon <ajagbegideon6@gmail.com>
2026-06-10 15:32:19 +0100

    Fix merge conflicts

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 const closeMenu = document.getElementById('close-menu');
@@ -10,16 +9,3 @@ hamburger.addEventListener('click', function() {
 closeMenu.addEventListener('click', function() {
   navLinks.classList.remove('open');
 });
-=======
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-const closeMenu = document.getElementById('close-menu');
-
-hamburger.addEventListener('click', function() {
-  navLinks.classList.toggle('open');
-});
-
-closeMenu.addEventListener('click', function() {
-  navLinks.classList.remove('open');
-});
->>>>>>> cca3bdeb50ff289eb41c2839300cb8513539a71f
